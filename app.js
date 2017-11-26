e1d5b56916f7a58117af4df0063540861127c8ae
Davy'd <pdaswift3009@gmail.com>
2017-11-26 11:57:29 +0100

    add layout profile routes

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
 
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/', users);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,17 @@ var router = express.Router();
 var User = require('../models/user');
 
 
+router.get('/login', function(req, res,next){
+  res.render('profiles/login')
+})
 
 /* GET users listing. */
+
+router.get('/register', function(req, res,next){
+   res.render('profiles/register')
+})
+
+/* POST users listing. */
 router.post('/register', function(req, res, next) {
   // res.send('hi');
   var user = new User();
@@ -14,9 +23,15 @@ router.post('/register', function(req, res, next) {
   user.password = req.body.password;
   user.email = req.body.email;
 
-  user.save(function (err){
-     if(err) return next(err);
-     res.json('Successfully created new user')
+  User.findOne({email : req.body.email}, function(err, userExist){
+    if(userExist){
+        console.log('User already exist!!!!!!!');
+       return res.redirect('register');
+    }
+      user.save(function (err, user){
+         if(err) return next(err);
+         res.json('Successfully created new user!!!');
+      });
   })
 });
 
